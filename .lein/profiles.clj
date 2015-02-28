b3a2166cbad5cd70de2a549d14fcb04ac334f9fd
David Jarvis <david@standardtreasury.com>
2015-02-27 21:21:40 -0800

    [lein] Updating .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,10 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-bikeshed "0.2.0"]
-                  [lein-cljfmt "0.1.4"]
+                  [lein-pprint "1.1.2"]
+                  [lein-cljfmt "0.1.10"]
                   [jonase/eastwood "0.2.1"]
-                  [venantius/ultra "0.2.0"]]
-        :dependencies [[cljfmt "0.1.7"]
+                  [venantius/ultra "0.2.1"]]
+        :dependencies [[cljfmt "0.1.10"]
                        [jonase/eastwood "0.2.1" :exclusions  [org.clojure/clojure]]]
         :ultra {:color-scheme :solarized_dark}
         :eastwood {:exclude-linters [:constant-test :suspicious-expression :local-shadows-var]}
