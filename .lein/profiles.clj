0e0e5dde644c9999603e9c194a6bb252e0444e0d
Ursa Americanus Kermodei <venantius@gmail.com>
2015-11-01 11:36:21 -0800

    [lein] Adding ClojureScript-related tooling
    New plugins:
     - cider-nrepl
     - ultra
    
    Updated plugins:
     - lein-kibit
     - lein-cljfmt
    
    New dependencies:
     - piggieback
     - tools.nrepl
     - eastwood
    
    Added piggieback nrepl-middleware

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,14 +1,21 @@
-{:user {:plugins [[lein-kibit "0.0.8"]
+{:user {:plugins [[lein-kibit "0.1.2"]
                   [lein-bikeshed "0.2.0"]
-                  [lein-cljfmt "0.1.4"]
+                  [lein-cljfmt "0.2.0"]
                   [jonase/eastwood "0.2.1"]
                   [lein-pprint "1.1.1"]
-                  [venantius/yagni "0.1.2"]]
+                  [cider/cider-nrepl "0.9.1"]
+                  [venantius/yagni "0.1.5-SNAPSHOT"]
+                  [venantius/ultra "0.4.0"]
 
-        :dependencies [[cljfmt "0.1.12"]
-                       [jonase/eastwood "0.2.1" :exclusions  [org.clojure/clojure]]]
+                  ]
 
-        :repl-options {:prompt (fn [ns] (str "[" ns "](\u001b[36mλ\u001b[0m)> "))}
+        :dependencies [[cljfmt "0.2.0"]
+                       [com.cemerick/piggieback "0.2.1"]
+                       [org.clojure/tools.nrepl "0.2.10"]
+                       [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
+
+        :repl-options {:prompt (fn [ns] (str "[" ns "](\u001b[36mλ\u001b[0m)> "))
+                       :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
         :ultra {:color-scheme :solarized_dark
                 :stacktraces false}
