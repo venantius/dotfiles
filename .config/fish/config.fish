bf04bdfce9b83da2781b13be0c31104a13792968
Ursa Americanus Kermodei <venantius@gmail.com>
2016-08-28 10:50:40 -0700

    [fish|gpg] Add GPG agent hunting to config.fish
    Stolen from
    https://blog.erincall.com/p/signing-your-git-commits-with-gpg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,16 @@ alias gst='git status'
 
 set -x GOPATH $HOME/Code/go
 set -x GO15VENDOREXPERIMENT 1
+
+if not begin
+    # Is the agent running already? Does the agent-info file exist, and if so,
+    # is there a process with the pid given in the file?
+    [ -f ~/.gpg-agent-info ]
+    and kill -0 (cut -d : -f 2 ~/.gpg-agent-info) ^/dev/null
+end
+    # no, it is not running. Start it!
+    gpg-agent --daemon --no-grab --write-env-file ~/.gpg-agent-info >/dev/null ^&1
+end
+# get the agent info from the info file, and export it so GPG can see it.
+set -gx GPG_AGENT_INFO (cut -c 16- ~/.gpg-agent-info)
+set -gx GPG_TTY (tty)
